d50c352e157a9847584aecd0fa4e27e278afec47
Malte Bär <maltebaer@posteo.de>
2021-02-20 19:01:38 +0100

    Init project.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="mt-4 text-4xl text-green-500 font-bold text-center">
+    Hello world
+  </h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
